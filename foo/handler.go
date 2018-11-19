e242f855b310b3e8a0b4590cb938edcb3af27ba7
Kelsey Hightower <kelsey.hightower@gmail.com>
2018-11-19 00:31:34 +0000

    update foo

--- foo/handler.go
+++ foo/handler.go
@@ -2,9 +2,24 @@ package foo
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	response, err := http.Get("http://127.0.0.1/bar")
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	defer response.Body.Close()
+
+	fmt.Fprintf(w, string(data))
 	fmt.Fprintf(w, "Foo Service")
 }
